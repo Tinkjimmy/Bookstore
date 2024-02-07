639d515f6551f1036640b677f549ea867ff6a4b2
Tinkjimmy <tinkjimmy938@gmail.com>
2024-02-07 22:58:43 +0100

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://frozen-forest-13105-34ae5fa7efd5.herokuapp.com/']
+ALLOWED_HOSTS = ['frozen-forest-13105-34ae5fa7efd5.herokuapp.com']
 
 
 # Application definition
